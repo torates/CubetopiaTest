7b429cc841b8f2a9bbf26ffe638baf6060433ef4
torates <goddengit5@gmail.com>
2022-03-28 16:41:22 +0200

    Initial commit.

---
+++ src/Cubetopia_assets/src/index.js
@@ -0,0 +1,19 @@
+import { Cubetopia } from "../../declarations/Cubetopia";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await Cubetopia.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
